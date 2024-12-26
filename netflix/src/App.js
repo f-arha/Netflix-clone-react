9c98b2aa0c9a60035bf9822290d1c3b4c9165475
f-arha <190675045+f-arha@users.noreply.github.com>
2024-12-26 14:22:55 +0530

    make Rowpost dynamic

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,13 +3,18 @@ import NavBar from './Components/NavBar/NavBar';
 import './App.css'
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
+import {originals,actions,comedy,romantic,horror} from './urls'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
-      <RowPost />
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={actions} title='Action' isSmall />
+      <RowPost url={comedy} title='Comedy' isSmall />
+      <RowPost url={horror} title='Horror' isSmall />
+      <RowPost url={romantic} title='Romantic' isSmall />
     </div>
   );
 }
